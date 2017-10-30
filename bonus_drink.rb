b1cc2888031463b98460df1bd007692f991d98d0
Hiroshi Ueki <hirocueki@gmail.com>
2017-10-30 16:41:29 +0900

    その他のテストを追加し、実装

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,9 +1,20 @@
 class BonusDrink
+  # 3本買うごとに1本おまけがもらえる
   def self.total_count_for(amount)
-    if amount == 0
-      0
-    elsif amount==1
-      1
+    if amount/3 == 0
+      return amount
     end
+
+    bonus = 0
+    akibin = amount
+    while (akibin/3) >0 do
+      change = akibin/3
+      akibin = akibin%3 + change
+      if (akibin/3) == 0
+        break
+      end
+      bonus += change
+    end
+    amount + bonus + akibin
   end
 end
